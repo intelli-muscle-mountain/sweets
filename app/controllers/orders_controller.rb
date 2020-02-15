9d2a42ea6751250e926f54c562728f05ba3a9b7b
yuhidomoto <yuhi4424@yahoo.co.jp>
2020-02-15 12:32:58 +0000

    order-item

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,22 @@
 class OrdersController < ApplicationController
 
 	def new
-	@order = Order.new
-  @total_price = 0
-  address = Address.all
 	end
 
 	def create
-		@order = Order.new
+			order = Order.new(order_params)
+		if order.save
+			current_customer.cartitems.each do | item |
+				order_item =OrderItem.new(order_id: order.id)
+				order_item.item_id = item.item_id
+				order_item.price = item.item.price
+				order_item.quantity = item.quantity
+				order_item.save
+
+			end
+		end
+			current_customer.cartitems.destroy_all
+			redirect_to thanks_orders_path
 	end
 
 	def index
@@ -20,12 +29,31 @@ class OrdersController < ApplicationController
 	end
 
 	def confirm
-		@order = Order.new(order_params)
-		@orders = Order.where(customer_id: current_customer.id)
+			@total_price = 0
+			@order = Order.new
+			@order.payment = params[:payment]
+			@cartitems = Cartitem.where(customer_id: current_customer.id)
+		if params[:address] == "新しいお届け先"
+			@order.postal_code = params[postal_code]
+			@order.address = params[new_address]
+			@order.destination_name = params[destination_name]
+		elsif params[:address] == "選択住所"
+			address = Address.find_by(id: params[address_id])
+			@order.postal_code = address.postal_code
+			@order.postal_code = address.address
+			@order.postal_code = address.destination_name
+		elsif params[:address] == "ご自身の住所"
+			@order.postal_code = current_customer.postal_code
+			@order.address = current_customer.address
+			@order.destination_name = current_customer.last_name
+		end
 	end
 
 def order_params
-params.require(:order).permit(:customer_id,:payment,:freight,:order_status,:total_price,:destination_name,:postal_code,:address)
+	params.require(:order).permit(:customer_id,:payment,:freight,:order_status,:total_price,:destination_name,:postal_code,:address)
 end
-	
+
+def order_item_params
+	params.require(:order_item).permit(:order_id,:item_id,:price,:quantity,:item_status)
+end
 end
