44aa2a796a8b45e0a050dc58b4a5a4b55ddcee41
YoshidaTatsumi <yoichi_1308@yahoo.co.jp>
2020-02-17 09:05:44 +0000

    [fix]layout

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,14 @@
 class Address < ApplicationRecord
 
 	belongs_to :customer
-validates :destination_name, presence: true
-validates :postal_code, presence: true
-validates :address, presence: true
+	validates :destination_name, presence: true
+	validates :postal_code, presence: true
+	validates :address, presence: true
+
+	def name_address
+		"〒" + self.postal_code + " " + self.address + " " + self.destination_name
+	end
+
+
+
 end
