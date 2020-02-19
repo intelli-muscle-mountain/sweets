c43f0e45a245eac3705735d7e1ff07f81826eb6a
YoshidaTatsumi <yoichi_1308@yahoo.co.jp>
2020-02-19 06:58:35 +0000

    [fix]paginator

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,9 +6,15 @@ class ItemsController < ApplicationController
 		@genres = Genre.where(genre_status: true)
 		if params[:category].present?
 			@genre = @genres.find_by(genre_name: params[:category])
-			@items = Item.where(genre_id: @genre.id, sale_status: true).page(params[:page]).reverse_order
+			@items = Item.where(genre_id: @genre.id, sale_status: true).page(params[:page]).per(8)
+			items = Item.where(genre_id: @genre.id, sale_status: true)
+			@items_quantity = items.length
+			# @items = Item.page(params[:page]).per(8)
 		else
-			@items = Item.joins(:genre).where(sale_status: true, genres:{genre_status: true}).page(params[:page]).reverse_order
+			@items = Item.joins(:genre).where(sale_status: true, genres:{genre_status: true}).page(params[:page]).per(8)
+			items = Item.joins(:genre).where(sale_status: true, genres:{genre_status: true})
+			@items_quantity = items.length
+			# @items = Item.page(params[:page]).per(8)
 		end
 	end
 
